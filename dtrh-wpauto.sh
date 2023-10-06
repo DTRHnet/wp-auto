f845d79069340daa7035ce590ff81a7ba5e4a104
DTRHnet <145516411+DTRHnet@users.noreply.github.com>
2023-10-06 11:59:53 -0400

    Quite a few updates
    I've been cleaning this up, condensing it down, making it more efficient and less code. Still work in progress. Just need to add sql functionality.
    Will update properly in README.md

--- dtrh-wpauto.sh
+++ dtrh-wpauto.sh
@@ -4,7 +4,7 @@
 #  ------------------------------------
 #    ██████╗ ████████╗██████╗ ██╗  ██╗     Title      : Wordpress Auto Installer
 #    ██╔══██╗╚══██╔══╝██╔══██╗██║  ██║     Filename   : dtrh-wpauto.sh
-#    ██║  ██║   ██║   ██████╔╝███████║     Version    : 1.0.0
+#    ██║  ██║   ██║   ██████╔╝███████║     Version    : 1.1.0
 #    ██║  ██║   ██║   ██╔══██╗██╔══██║     Date       : 09.09.23
 #    ██████╔╝   ██║   ██║  ██║██║  ██║
 #    ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝     Additional : README.md
@@ -28,56 +28,36 @@ __file="${__dir}/$(basename "${BASH_SOURCE}")"
 __base="$(basename "${__file}" .sh)"
 __me="${BASH_SOURCE}"
 
-# Initialize global variables
-__HELP__=0
-__NO_BANNER__=0
-__COLOR__=1
-__VERBOSE__=0
-__INTERACTIVE__=0
-__UNEXPECTED__=0
-__LOG_FILE__="/tmp/wpauto.log"
-__bad_opt__=""
-__INSTALL_DIR__=""
-__INSTALL_USR__=""
-declare -a __ARGS__
+# Global variable initialization
+__help="0"
+wp_admin=""
+wp_dir=""
+_log="0"
+log_file="/tmp/wp_auto.log"
+bad_opt=""
+declare -a sh_args
+sql_user=""
+sql_pass=""
+sql_wpdb=""
+wp_host=""
 
-
-# Define colour variables
-NC="\033[0m"
-BNC="\033[1m"
-BLK="\033[0;30m"
-BBLk="\033[1;30m"
-RED="\033[0;31m"
-BRED="\033[1;31m"
-GRN="\033[0;32m"
-BGRN="\033[1;32m"
-YLW="\033[0;33m"
-BYLW="\033[1;33m"
-BLU="\033[0;34m"
-BBLU="\033[1;34m"
-PPL="\033[0;35m"
-BPPL="\033[1;35m"
-CYN="\033[0;36m"
-BCYN="\033[1;36m"
 
 # Root check
-if [ ${EUID} -ne 0 ]; then
-  echo -n -e "${BYLW}[${__base^^}] ${BRED}ERROR:\n${NC}Run script with ${BNC}root ${NC}privileges!"
-  exit
-fi
-
-# Function definitions -----------------------------------------------------------------------------
+[ ${EUID} -ne 0 ] &&  echo -e "${__base^^}\n\n ERROR:\nRun script with root privileges!\n\n"
+case "${EUID}" in
+  0) echo "Checking for root priviliges..."; sleep "0.5"; echo -e "Done.\n\n";;
+  *) exit 1 ;;
+esac
 
 # Function : Banner - Display DTRH banner
 __banner() {
-  if [ ${__NO_BANNER__} = 0 ]; then
-    cat <<EOF 
+cat <<EOF 
 # ######################################
 #  ------------------------------------
 #    ██████╗ ████████╗██████╗ ██╗  ██╗     Title      : Wordpress Auto Installer
-#    ██╔══██╗╚══██╔══╝██╔══██╗██║  ██║     Filename   : dtrh-wpauto.sh
-#    ██║  ██║   ██║   ██████╔╝███████║     Version    : 0.0.1
-#    ██║  ██║   ██║   ██╔══██╗██╔══██║     Date       : 09.09.23
+#    ██╔══██╗╚══██╔══╝██╔══██╗██║  ██║     Filename   : wp_auto-1.1-0.sh
+#    ██║  ██║   ██║   ██████╔╝███████║     Version    : 1.1-0
+#    ██║  ██║   ██║   ██╔══██╗██╔══██║     Date       : 10.06.23
 #    ██████╔╝   ██║   ██║  ██║██║  ██║
 #    ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝     Additional : README.md
 #            https://dtrh.net
@@ -86,15 +66,13 @@ __banner() {
 
 
 EOF
-  fi
 }
 
 # Function : Usage - Display simple usage information
 __usage() {
   __banner
 cat <<EOF
-Usage: ${__me} [OPTION]... [FLAG]...  [USER]
-OR     ${__me} -i [OPTION]... [FLAG]...
+Usage: ${__me} [USER]
 
        
 EOF
@@ -103,59 +81,59 @@ EOF
 # Function : Help - Display detailed usage information 
 __help() {
   __banner
-  echo -n -e "${BYLW}[${__base^^}] ${BBLU}HELP:${NC}\n"
+  echo -e "${__base^^} HELP:\n"
   cat <<EOF
 
-Usage: ${__me} [OPTION]... [FLAG]... [USER]
-OR     ${__me} -i [OPTION]... [FLAG]...  
+Usage: ${__me} [USER]
 
+wp_auto.sh installs a fresh copy of the latest version of weordpress to
+your /var/www/html folder, in the subfolder named 'wordpress'. It takes
+one argument, a user name for the admin/ownef of the wordpress website
+being installed. 
+
+See README.md for more information on tweaking this script.
 
 Options:
   -l [FILE]      Create logfile. Write to [FILE]
-  
-Flags:
   -h             Display this text
-  -b             Disable banner    [Default: Disabled]
-  -c             Disable color     [Default: Disabled]
-  -i             Interactive Mode  [Default: Disabled]
-  -v             Verbose output    [Default: Disabled]
+  -v             Display script version
+
+
 EOF
-  exit 1
-}
+exit 1; }
 
 # getopts loop : Interpret user input & sort
-while getopts :bhl:icv opt; do
+while getopts :hl:v opt; do
   case "${opt}" in
-    b) __NO_BANNER__=1;;
-    h) __HELP__=1;; 
-    l) __LOG_FILE__="${OPTARG}";;
-    :) __UNEXPECTED__=2;;
-    i) __INTERACTIVE__=1;;
-    c) __NO_COLOR__=1;;
-    v) __VERBOSE__=1;;
-    \?) __UNEXPECTED__=1; __bad_opt__=${opt};;
+    h) __help="1";; 
+    l) log_file="${OPTARG}" && _log="1";;
+    :) bad_opt="2";;   # Missing log file
+    \?) bad_opt="1";;  # Unrecognized opt
   esac
 done
 
 # Shift through options index one by one
 shift $((${OPTIND} - 1))
 
-# TODO: This checks if nothing but the script is run (no user input otherwise).
-#       Because the array to hold arguments is of an unknown size, initializing it
-#       properly is problematic for the argument count. Fix. Likely use 'unset' 
-#       before counting args, and remove this snippet.
-if [ ${#} = 0 ]; then
-  __usage
-  echo -n -e "${BYLW}[${__base^^}] ${BRED}ERROR:\n"
-  echo -n -e "0 arguments supplied for non-interactive mode. Required: 2\n\n"
-  echo -n -e "${NC}Use ${BNC}[ -h ] ${NC}or see ${BNC}README.md${NC}\n"
-  exit 1
-fi
+#if [ ! command -v screen ] && [ ${_log} = "1" ]; then 
+#echo -e "You are missing a dependency: screen. The script will be sure to install it momentarily. Pleaae restart the script to #continue.." && exit 1;
+# elif [ command -v screen ]; then
+#   if [ ! -f ${log_file} ]; then 
+#     echo -e "\n\n$(date) - wp_auto log file" > ${log_file}
+#     screen -L -Logfile ${log_file}
+#fi
 
+if [ ${#} = 0 ]; then 
+  __usage
+  echo -e "$[{__base^^}] ERROR:\n\n"
+  echo -e "0 arguments were supplied. Required: 2\n\n"
+  echo -e "Use [ -h ] option or see README.md for more information"
+  exit 1
+ fi 
+ 
 # With all options parsed, store remaining arguments in an array
 while test ${#} -gt 0; do
-  __ARGS__[${#}]=$1
-  # echo ${__ARGS__[${#}]}
+  sh_args[${#}]=$1
   shift
 done
 
@@ -163,140 +141,147 @@ done
 # eg. ${__me} -h -b   will call help first before switching the no-banner flag
 #                     resulting in banner displaying. This is why functions are not
 #                     called directly in getopts loop.
-if [ ${__HELP__} = 1 ]; then 
+if [ ${__help} = 1 ]; then 
   __help
 fi
 
-# Options are parsed out, expectation is two arguments remain
-# Throw appropriate errors if array is not expected size
-if [ ! ${#__ARGS__[@]} = 1 ]; then 
+# Options are parsed out, only expect username..
+if [ ! ${#sh_args[@]} = 1 ]; then 
   __usage
-  echo -n -e "${BYLW}[${__base^^}] ${BRED}ERROR:\n"
-  echo -n -e "${#__ARGS__[@]} arguments supplied for non-interactive mode. Required: 1\n\n"
-  echo -n -e "${NC}Use ${BNC}[ -h ] ${NC}or see ${BNC}README.md${NC}\n"
+  echo -e "[${__base^^}] ERROR:\n"
+  echo -e "${#sh_args[@]} arguments supplied for non-interactive mode. Required: 1\n\n"
+  echo -e "Use [ -h ] or see README.md\n"
   exit 1
 fi
 
 # Check for unexpected input errors among the options and throw appropriate errors
-if [ ! ${__UNEXPECTED__} = 0 ]; then
+if [ ! ${bad_opt} = 0 ]; then
   __usage
-  echo -n -e "${BYLW}[${__base^^}] ${BRED}ERROR:\n"
-    if [ ${__UNEXPECTED__} = 1 ]; then
-      # It turns out invalid opts are indexed as '?'. This was an attempt to return
-      # the invalid option and display it for the user. 
-      # TODO : Fix or remove
-      echo -n -e "${RED}Unknown option(s): ${BNC}${__bad_opt__}\n\n"
-    elif [ ${__UNEXPECTED__} = 2 ]; then
-      echo -n -e "${RED}Missing mandatory log file parameter${NC}\n\n"
+  echo  -e "[${__base^^}] ERROR:\n"
+    if [ ${bad_opt} = 1 ]; then
+      echo  -e "Unknown option(s)\n\n"
+    elif [ ${bad_opt} = 2 ]; then
+      echo  -e "Missing mandatory log file parameter\n\n"
     fi
-    echo -n -e "${NC}Use ${BNC}[ -h ] ${NC}or see ${BNC}README.md${NC}\n"
-    exit 1
+  echo  -e "Use [ -h ] or see README.md\n"
+  exit 1
 fi
 
-# Main entry
+# Most checking complete, time to do things..
 __banner
 
-echo -n -e "${BYLW}[${__base^^}] ${BNC}: Updating package index & upgrading packages..\n${NC}"
+echo -e "[${__base^^}] : Updating package index & upgrading packages..\n"
 apt-get update && apt-get upgrade -y
-echo -n -e "${BNC}Done!${NC}\n\n"
+echo -e "Done!\n\n"
 sleep .5
 
-echo -n -e "${BYLW}[${__base^^}] ${BNC}: Installing dependencies..\n${NC}"
+echo -e "[${__base^^}] : Installing dependencies..\n"
 apt install apache2 \
-  ghostscript \
-  libapache2-mod-php \
-  php \
-  php-bcmath \
-  php-curl \
-  php-imagick \
-  php-intl \
-  php-json \
-  php-mbstring \
-  php-mysql \
-  php-xml \
-  php-zip
-echo -n -e "${BNC}Done!${NC}\n\n"
+            curl \
+            ghostscript \
+            libapache2-mod-php \
+            php \
+            mysql-server \
+            php-bcmath \
+            php-curl \
+            php-imagick \
+            php-intl \
+            php-json \
+            php-mbstring \
+            php-mysql \
+            php-xml \
+            php-zip -y
+echo -e "Done!\n\n"
 sleep .5
 
-# TODO : Having issues with kali
-# echo -n -e "${BYLW}[${__base^^}] ${BNC}: Installing MySQL server..\n${NC}"
-# mysql_secure_installation
-# echo -n -e "${BNC}Done!${NC}\n\n"
-# sleep .5
+echo -e "[${__base^^}] : Installing MySQL server..\n"
+echo -e "The script will now ask a few questions to properly set up the wordpress database and associated accounts/passwords\n"
+mysql_secure_installation 
+echo -e "Name of database user/admin: "; read sql_user
+echo -e "Password for user ${sql_user}: "; read sql_pass
+echo -e "Wordpress database name: "; read sql_wpdb
+echo -e "Database host is 'localhost "; db_host="localhost"
+sleep .5
+echo -e "done.\n"
 
-echo -n -e "${BYLW}[${__base^^}] ${BNC}: Setting up user and directory..\n${NC}"
-__INSTALL_USR__=${__ARGS__[1]}
-__INSTALL_DIR__="/var/www/html"
+echo  -e "[${__base^^}] : Setting up user and directory..\n"
+wp_usr=${sh_args[1]}
+wp_dir="/var/www/html"
 sleep .5
 
-if id -u ${__INSTALL_USR__} >/dev/null 2>&1; then
-  echo -n -e "User ${BNC}${__INSTALL_USR__}${NC} exists. Adding to ${BNC}www-data${NC} group.\n"
-  # useradd -g www-data ${__INSTALL_USR__}
+if id -u ${wp_usr} >/dev/null 2>&1; then
+  echo  -e "User ${wp_usr} exists. Adding to www-data group.\n"
+  usermod -aG www-data ${wp_usr}
 else
-  echo -n -e "User ${BNC}${__INSTALL_USR__}${NC} doesn't exist. Creating user and adding to ${BNC}www-data${NC} group.\n"
-  # useradd -g www-data ${__INSTALL_USR__}
+  echo  -e "User ${wp_usr} doesn't exist. Creating user and adding to www-data group.\n"
+  useradd -g www-data ${wp_usr}
+  usermod -aG www-data sudo ${wp_usr}
 fi
 sleep .5
 
-if [ -d ${__INSTALL_DIR__} ]; then
-  echo -n -e "Directory ${BNC}${__ARGS__[1]}${NC} exists.. Setting permissions to ${BNC}${__INSTALL_DIR__}${NC} for user ${BNC}${__INSTALL_USR__}${NC}\n"
+if [ -d ${wp_dir} ]; then
+  echo -e "Directory ${sh_args[1]} exists.. Setting permissions for ${wp_dir} and user ${wp_usr}\n"
 else
-  echo -n -e "Directory ${BNC}${__ARGS__[1]}${NC} does not exist.. Creating and setting permissions to ${BNC}${__INSTALL_DIR__}${NC} for user ${BNC}${__INSTALL_USR__}${NC}\n"
-  mkdir -p ${__INSTALL_DIR__}
+  echo  -e "Directory ${sh_args[1]} does not exist.. Creating directory ${wp_dir} and fixing permissions for user ${wp_usr}\n"
+  mkdir -p ${wp_dir}
 fi
 
-chown ${__INSTALL_USR__}: ${__INSTALL_DIR__}
+chown ${wp_usr}: ${wp_dir}
 sleep .5
-echo -n -e "${BNC}Done!${NC}\n\n"
+echo  -e "Done!\n\n"
 
-echo -n -e "${BYLW}[${__base^^}] ${BNC}: Configuring Apache web server..\n${NC}"
+echo  -e "[${__base^^}]: Configuring Apache web server..\n"
 cat <<EOF > /etc/apache2/sites-available/wordpress.conf
 <VirtualHost *:80>
-    DocumentRoot ${__INSTALL_DIR__}/wordpress
-    <Directory ${__INSTALL_DIR__}/wordpress>
+    DocumentRoot ${wp_dir}/wordpress
+    <Directory ${wp_dir}/wordpress>
         Options FollowSymLinks
         AllowOverride Limit Options FileInfo
         DirectoryIndex index.php
         Require all granted
     </Directory>
-    <Directory ${__INSTALL_DIR__}/wordpress/wp-content>
+    <Directory ${wp_dir}/wordpress/wp-content>
         Options FollowSymLinks
         Require all granted
     </Directory>
 </VirtualHost>
 EOF
-echo -n -e "${NC}Created file: ${BNC}wordpress.conf${NC} in directory ${BNC}/etc/apache2/sites-available${NC}\n"
-echo -n -e "${BNC}Done!${NC}\n\n"
+
+echo -e "Creating apache configuration filr wordpress.conf in directory /etc/apache2/sites-available\n"
+echo  -e "Done!\n\n"
 sleep .5
 
-echo -n -e "${BYLW}[${__base^^}] ${BNC}: Downloading and extracting wordpress..\n${NC}"
-echo -n -e "Downloading latest version of wordpress and extracting to ${BNC}${__INSTALL_DIR__}${NC} in subfolder ${BNC}wordpress${NC}\n"
-echo -n -e "Working ...\n\n"
-curl -sL -o- https://en-ca.wordpress.org/latest-en_CA.tar.gz | sudo -u ${__INSTALL_USR__} tar -xz -C ${__INSTALL_DIR__}
+echo  -e "[${__base^^}] : Downloading and extracting wordpress..\n"
+echo  -e "Downloading latest version of wordpress and extracting to ${wp_dir} in subfolder wordpress\n"
+echo  -e "Working ...\n\n"
+curl -sL -o- https://en-ca.wordpress.org/latest-en_CA.tar.gz | sudo -u ${wp_usr} tar -xz -C ${wp_dir}
 sleep .5
-echo -n -e "${BNC}Done!${NC}\n\n"
+echo  -e "Done!\n\n"
 
-echo -n -e "${BYLW}[${__base^^}] ${BNC}: Restarting Apache web server..\n${NC}"
+echo  -e "[${__base^^}] : Restarting Apache web server..\n"
 a2enmod rewrite
-# a2ensite wordpress
+a2ensite wordpress
 service apache2 reload
-echo -n -e "${BNC}Done!${NC}\n\n"
+echo  -e "Done!\n\n"
 
-echo -n -e "${BYLW}[${__base^^}] ${BNC}: Setting final permissions..\n${NC}"
-chmod -R 755 ${__INSTALL_DIR__}/wordpress
+
+# Print Results 
+
+echo  -e "[${__base^^}] : Setting final permissions..\n"
+chmod -R 755 ${wp_dir}/wordpress
 sleep .5
-echo -n -e "${BNC}Done!${NC}\n\n\n"
+echo  -e "Done!\n\n\n"
 
-echo -n -e "${BYLW}[${__base^^}] ${BNC}: RESULTS\n\n${NC}."
-echo -n -e "${NC}Wordpress installation folder: ${BNC}${__INSTALL_DIR__}${NC}\n"
-echo -n -e "${NC}Website owner: ${BNC}${__INSTALL_USR__}${NC}\n"
-echo -n -e "${NC}Apache configuration: ${BNC}/etc/apache2/sites-available/wordpress.conf${NC}\n"
-echo -n -e "${NC}Local address: ${BNC}http://127.0.0.1/wordpress${NC}\n\n"
+echo  -e "[${__base^^}] : RESULTS\n\n."
+echo  -e "Wordpress installation folder: ${wp_dir}.\n"
+echo  -e "Website owner: ${wp_usr}.\n"
+echo  -e "Apache configuration: /etc/apache2/sites-available/wordpress.conf\n"
+echo -e "MySql username; ${sql_user}"
+echo -e "MySql Password: ${sql_pass}"
+echo -e "Wordpress DB: ${sql_wpdb}"
+echo -e "DB host: ${sql_host}"
+echo  -e "Local address: http://127.0.0.1/wordpress\n\n"
 
-echo -n -e "${BYLW}This script is not complete. Please be sure to read ${BNC}README.md ${BYLW}for full details\n${BNC}$(date)"
 
-
-
-
-
+echo  -e "This script is not complete. Please be sure to read README.md for full details\n$(date)"
 
